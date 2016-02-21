04d39c8225970364166ef62eb62f7cfcf8af5684
Marcin Dudek <manekd@gmail.com>
2016-02-21 17:16:02 +0100

    After download build succeeds. Improved Generation tests

--- Fibonacci_Generate_test.go
+++ Fibonacci_Generate_test.go
@@ -4,29 +4,12 @@ import "testing"
 
 // TestGeneration this function tests several test cases
 func TestGeneration(t *testing.T) {
-	cases := []struct{ in, expected int }{
-		{0, 0},
-		{1, 1},
-		{2, 1},
-		{3, 2},
-		{4, 3},
-		{5, 5},
-		{6, 8},
-		{7, 13},
-		{8, 21},
-		{9, 34},
-		{10, 55},
-		{11, 89},
-		{12, 144},
-		{13, 233},
-		{14, 377},
-		{15, 610},
-	}
+	numbers := []int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610}
 	var fib Fibonacci
 	f := fib.Generate()
-	for _, c := range cases {
-		if result := f(); result != c.expected {
-			t.Errorf("Expected %d, got %d", result, c.expected)
+	for _, number := range numbers {
+		if result := f(); result != number {
+			t.Errorf("Expected %d, got %d", number, result)
 		}
 	}
 }
--- fibonacci.go
+++ fibonacci.go
@@ -5,8 +5,12 @@ type Fibonacci struct{}
 
 // Get is a function which return n-th fibonacci number
 func (f Fibonacci) Get(n int) int {
+	return 0
 }
 
 // Generate is a function which generates consecutive numbers.
 func (f Fibonacci) Generate() func() int {
+	return func() int {
+		return 0
+	}
 }
