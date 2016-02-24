2e291eb6ea3c261bcdb73d347c661b00dda1d9a6
Marcin Dudek <manekd@gmail.com>
2016-02-24 23:23:10 +0100

    Added hints to gorutines

--- Routines/Program.go
+++ Routines/Program.go
@@ -14,9 +14,13 @@ func main() {
 	fmt.Printf("We are missing: %d\n", expected-sum)
 }
 
+// sumToChannel is a function that will sum input and save result into channel
+// hint: this is synchrounous function, will be called with 'go' keyword
 func sumToChannel(x []int, c chan int) {
 }
 
+// gosum is function that returns channel with sum (of input array)
+// hint: this function will trigger async computations and return channel
 func gosum(x []int) <-chan int {
 	return make(chan int)
 }
